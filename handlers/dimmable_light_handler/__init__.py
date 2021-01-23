4c24bd96f7bb0a1fcd954cbc5697a168174b5606
Niklas Dörfler <niklas@doerfler-el.de>
2021-01-23 12:06:25 +0100

    Feature: Add dimmable light handler (#1)
    * Add dimmable-light-handler for one channel light
    * Fix device value range parameter to be parsed as int
    * Add endless loop to keep program running
    * Add pycache to .gitignore

--- handlers/dimmable_light_handler/__init__.py
+++ handlers/dimmable_light_handler/__init__.py
@@ -7,30 +7,30 @@ from core.mqtt_connector import MqttConnector
 
 FADE_STEPS = 60
 
-MQTT_TOPIC_COLOR_SUFFIX = "color"
+MQTT_TOPIC_BRIGHTNESS_SUFFIX = "brightness"
 MQTT_TOPIC_POWER_SUFFIX = "power"
 
 
-class ColorLightHandler:
+class DimmableLightHandler:
     def __init__(self, mqtt_connector: MqttConnector, pwm: PiPwm, mqtt_basetopic: str,
-                 id: str, channel_ids: (), value_range: int):
+                 id: str, channel_id: int, value_range: int):
         self.__id = id
-        self.__channel_ids = channel_ids
+        self.__channel_ids = (channel_id,)
         self.__pwm = pwm
         self.__value_range = value_range
 
         self.__fade_thread = None
         self.__stop_thread_trigger = False
 
-        self.__current_rgb_values = (0,) * len(channel_ids)
-        self.__last_state_rgb_values = (0,) * len(channel_ids)
+        self.__current_rgb_values = (0,) * len(self.__channel_ids)
+        self.__last_state_rgb_values = (0,) * len(self.__channel_ids)
         self.__light_state_on = False
 
-        logging.debug("Init LedStripLight with name %s", id)
+        logging.debug("Init DimmableLight with name %s", id)
         mqtt_connector.subscribe_to_topic(mqtt_basetopic + "/" + id + "/" + MQTT_TOPIC_POWER_SUFFIX,
                                           self.__handle_power_command)
-        mqtt_connector.subscribe_to_topic(mqtt_basetopic + "/" + id + "/" + MQTT_TOPIC_COLOR_SUFFIX,
-                                          self.__handle_color_command)
+        mqtt_connector.subscribe_to_topic(mqtt_basetopic + "/" + id + "/" + MQTT_TOPIC_BRIGHTNESS_SUFFIX,
+                                          self.__handle_brightness_command)
 
     def is_fading(self):
         return self.__fade_thread is not None and self.__fade_thread.is_alive()
@@ -38,20 +38,20 @@ class ColorLightHandler:
     def is_on(self):
         return self.__light_state_on
 
-    def fade_to_colors(self, rgb_state: (int, int, int)):
+    def fade_to_brightness(self, brightness: int):
         if self.is_fading():
             self.__stop_fading()
 
         logging.debug("Start fading for light '%s'...", self.__id)
         self.__stop_thread_trigger = False
-        self.__fade_thread = Thread(target=self.__do_fade, args=(rgb_state, lambda: self.__stop_thread_trigger))
+        self.__fade_thread = Thread(target=self.__do_fade, args=(brightness, lambda: self.__stop_thread_trigger))
         self.__fade_thread.start()
 
-    def get_current_rgb_values(self):
-        return self.__current_rgb_values
+    def get_current_brightness(self):
+        return self.__current_rgb_values[0]
 
-    def get_channel_ids(self):
-        return self.__channel_ids
+    def get_channel_id(self):
+        return self.__channel_ids[0]
 
     def __handle_power_command(self, message):
         light_state = str(message.decode("utf-8"))
@@ -59,23 +59,18 @@ class ColorLightHandler:
 
         if light_state == "ON":
             self.__light_state_on = True
-            self.fade_to_colors(self.__last_state_rgb_values)
+            self.fade_to_brightness(self.__last_state_rgb_values[0])
         else:
             self.__light_state_on = False
-            self.fade_to_colors((0, 0, 0))
+            self.fade_to_brightness(0)
 
-    def __handle_color_command(self, message):
-        color_state = str(message.decode("utf-8"))
-        logging.debug("Handle color command for light %s: %s", self.__id, color_state)
-        values = color_state.split(";")
-        new_colors = ()
-
-        for value in values:
-            new_colors = new_colors + (int(float(value)),)
-
-        self.__last_state_rgb_values = new_colors
+    def __handle_brightness_command(self, message):
+        msg = str(message.decode("utf-8"))
+        logging.debug("Handle color command for light %s: %s", self.__id, msg)
+        value = int(msg)
+        self.__last_state_rgb_values = (value,)
         if self.__light_state_on:
-            self.fade_to_colors(new_colors)
+            self.fade_to_brightness(value)
 
     def __stop_fading(self):
         if self.is_fading():
